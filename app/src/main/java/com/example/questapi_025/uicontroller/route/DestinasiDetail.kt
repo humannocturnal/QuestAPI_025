dd3390737f8b2a130dd6ccee5d813a48b41ca821
humannocturnal <syaputrar374@gmail.com>
2025-12-27 12:28:04 +0700

    menambahkan interface destinasi navigasi

--- app/src/main/java/com/example/questapi_025/uicontroller/route/DestinasiDetail.kt
+++ app/src/main/java/com/example/questapi_025/uicontroller/route/DestinasiDetail.kt
@@ -5,7 +5,7 @@ import com.example.questapi_025.R
 object DestinasiDetail: DestinasiNavigasi {
     override val route = "detail_siswa"
 
-    override val titleRes = R.string.detail_siswa
+    override val titleRes = "Detail Siswa"
     const val itemIdArg = "idSiswa"
 
     val routeWithArgs = "$route/{$itemIdArg}"
--- app/src/main/java/com/example/questapi_025/uicontroller/route/DestinasiEntry.kt
+++ app/src/main/java/com/example/questapi_025/uicontroller/route/DestinasiEntry.kt
@@ -4,5 +4,5 @@ import com.example.questapi_025.R
 
 object DestinasiEntry : DestinasiNavigasi {
     override val route = "item_entry"
-    override val titleRes = R.string.entry_siswa
+    override val titleRes = entry_siswa
 }
